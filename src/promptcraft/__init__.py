b034aefcc6d8de7904300f11863aa54df3f6ef46
PromptCraft <promptcraft@users.noreply.github.com>
2026-06-01 15:02:27 +0800

    feat: add PromptBuilder with fluent API for prompt construction

---
+++ src/promptcraft/__init__.py
@@ -0,0 +1,19 @@
+﻿"""PromptCraft - A lightweight toolkit for prompt engineering."""
+
+__version__ = "0.1.0"
+__author__ = "PromptCraft Contributors"
+
+from promptcraft.builder import PromptBuilder
+from promptcraft.template import Template
+from promptcraft.optimizer import Optimizer
+from promptcraft.evaluator import Evaluator, EvalResult
+from promptcraft.scorer import Scorer
+
+__all__ = [
+    "PromptBuilder",
+    "Template",
+    "Optimizer",
+    "Evaluator",
+    "EvalResult",
+    "Scorer",
+]
